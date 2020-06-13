4268be1b0f001f0d70bd18f2dc904e26bc11015b
Piotr Sosno <p.sosno@student.mini.pw.edu.pl>
2020-06-14 00:56:05 +0200

    Callbacks

--- models.py
+++ models.py
@@ -1,7 +1,16 @@
+import os
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Flatten, Conv2D, MaxPooling2D, Dense, Dropout, SpatialDropout2D
 
+
+# This function keeps the learning rate at 0.001 for the first ten epochs
+# and decreases it exponentially after that.
+def scheduler(epoch):
+  if epoch < 10:
+    return 0.001
+  else:
+    return 0.001 * tf.math.exp(0.1 * (10 - epoch))
 
 def simple_cnn(input_shape, conv_layers,
                kernel_size=3, pool_size=2,
@@ -37,6 +46,33 @@ def simple_cnn(input_shape, conv_layers,
 
     model = Sequential()
     
+    
+    EPOCHS = 100
+    working_directory = os.path.dirname(os.getcwd())
+    checkpoint_filepath = os.path.join(working_directory, "Checkpoint") 
+    
+    # Model weights are saved at the end of every epoch, if it's the best seen so far.
+    # The model weights (that are considered the best) are loaded into the model.
+    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=False,
+    monitor='val_acc',
+    mode='max',
+    save_best_only=True)
+    
+    model_learningScheduler_callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
+    
+    model_earlyStopping_callback = tf.keras.callbacks.EarlyStopping(
+        monitor='loss', 
+        min_delta=0, 
+        patience=10,        #stopping training after 10 not improved consecutive epochs 
+        verbose=0, 
+        mode='auto',
+        baseline=None, 
+        restore_best_weights=False)
+    
+
+
     if regularization == 'spatial':
         regularization_layer = SpatialDropout2D(reg_rate)
     else:
@@ -54,5 +90,10 @@ def simple_cnn(input_shape, conv_layers,
     model.add(Flatten())
     model.add(Dense(100, activation='relu'))
     model.add(Dense(units=num_classes, activation='softmax'))
+    
+    
+    model.fit(epochs=EPOCHS, callbacks=[model_checkpoint_callback, model_learningScheduler_callback, model_earlyStopping_callback])
+    model.load_weights(checkpoint_filepath)
+
     
     return model
